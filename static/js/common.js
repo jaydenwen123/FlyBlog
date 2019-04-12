1518ad3cfe0a1c486c7e21a7e1f0caeaec6d887b
wenxiaofeiCode <2282186474@qq.com>
2019-04-12 09:44:44 +0800

    完善了index主页的内容，同时更新了用户、文章、栏目等几个模型数据 待完成的任务： 1.还需要对获取文章和栏目列表根据user_id进行过滤 2.根据user_id跳转到不同用户的主页 3.文章增加上传附件的功能 4.文章评论和留言功能 5.调整首页的布局 6.个人信息变更的功能的增加

---
+++ static/js/common.js
@@ -0,0 +1,41 @@
+function showCategory() {
+    //如果不设置false的话，会先提交表单，然后导致无法跳转
+    window.location.href="/category/show/"
+    return false
+}
+
+function showArticle() {
+    window.location.href="/article/show/"
+    return false;
+}
+function goAddCategory() {
+    window.location.href="/category/"
+    return false;
+}
+function goAddArticle() {
+    window.location.href="/article/"
+    return false;
+}
+
+function showTopBlock() {
+    layui.use(['layer', "jquery","util"], function () { //独立版的layer无需执行这一句
+        layer = layui.layer; //独立版的layer无需执行这一句
+        $= layui.jquery;
+        util=layui.util
+        util.fixbar({
+            top: true,
+            bar1:false,
+            bar2:false,
+            css:{right: 100, bottom: 100},
+            // bar1: true,
+            click: function (type) {
+                if(type==="bar2"){
+                    window.location.href="/about"
+                }
+            }
+        });
+        // sync=layui.sync;
+        //总页数大于页码总数
+    });
+
+}
